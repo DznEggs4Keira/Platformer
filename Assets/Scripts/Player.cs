7109559ca8bf8334636d9fa26d5aba2ff13f6936
Zainab Ali <47698736+DznEggs4Keira@users.noreply.github.com>
2021-07-05 21:08:12 +0400

    Revert "Merge branch 'dev' into main"
    This reverts commit 3508ba2d81417a88b5712816fcc806d79e0dbb7d, reversing
    changes made to 3ca80d8d65dc1c2861bf467ddd4bafd1360104ae.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRB;
-    [SerializeField] Animator playerAnim;
-    [SerializeField] SpriteRenderer playerSR;
-    [SerializeField] float _speed = 5f;
-    [SerializeField] float _jumpForce = 200f;
 
-    //you can avoid an if entirely with
-    //bool walking = horizontal != 0; - horizontal != 0 would be true, so anything other than that would be false
-    //bool flipX = horizontal < 0; - horizontal < 0 would be true, so anything other than that would be false
+    public float speed = 5f;
 
     // Start is called before the first frame update
     void Start() {
@@ -21,18 +15,9 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        var horizontal = Input.GetAxis("Horizontal") * _speed;
+        var horizontal = Input.GetAxis("Horizontal");
+        //var vertical = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(horizontal) >= 1) {
-            playerRB.velocity = new Vector2(horizontal, playerRB.velocity.y);
-            Debug.Log($"velocity = {playerRB.velocity}");
-        }
-
-        playerAnim.SetBool("isWalking", horizontal != 0);
-        if(horizontal != 0) playerSR.flipX = horizontal < 0;
-
-        if (Input.GetButtonDown("Jump")) {
-            playerRB.AddForce(Vector2.up * _jumpForce);
-        }
+        playerRB.velocity = new Vector2(horizontal * speed, playerRB.velocity.y);
     }
 }
