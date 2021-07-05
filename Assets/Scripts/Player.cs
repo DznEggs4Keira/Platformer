d9c26fb04beb3ba9627b799128ed1570ed0f02ef
Zainab Ali <47698736+DznEggs4Keira@users.noreply.github.com>
2021-07-05 18:50:22 +0400

    Lesson 2 - COMPLETE

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRB;
+    [SerializeField] Animator playerAnim;
+    [SerializeField] SpriteRenderer playerSR;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 200f;
 
-    public float speed = 5f;
+    //you can avoid an if entirely with
+    //bool walking = horizontal != 0; - horizontal != 0 would be true, so anything other than that would be false
+    //bool flipX = horizontal < 0; - horizontal < 0 would be true, so anything other than that would be false
 
     // Start is called before the first frame update
     void Start() {
@@ -16,8 +22,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update() {
         var horizontal = Input.GetAxis("Horizontal");
-        //var vertical = Input.GetAxis("Vertical");
 
         playerRB.velocity = new Vector2(horizontal * speed, playerRB.velocity.y);
+        playerAnim.SetBool("isWalking", horizontal != 0);
+        if(horizontal != 0) playerSR.flipX = horizontal < 0;
+
+        if (Input.GetButtonDown("Jump")) {
+            playerRB.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
