cac2d392c581c4e0016a9e3d1ababd4aeb299180
Zainab Ali <47698736+DznEggs4Keira@users.noreply.github.com>
2021-07-05 21:02:38 +0400

    Lesson 3 - COMPLETE

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,8 @@ public class Player : MonoBehaviour
     [SerializeField] Rigidbody2D playerRB;
     [SerializeField] Animator playerAnim;
     [SerializeField] SpriteRenderer playerSR;
-    [SerializeField] float speed = 5f;
-    [SerializeField] float jumpForce = 200f;
+    [SerializeField] float _speed = 5f;
+    [SerializeField] float _jumpForce = 200f;
 
     //you can avoid an if entirely with
     //bool walking = horizontal != 0; - horizontal != 0 would be true, so anything other than that would be false
@@ -21,14 +21,18 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
 
-        playerRB.velocity = new Vector2(horizontal * speed, playerRB.velocity.y);
+        if (Mathf.Abs(horizontal) >= 1) {
+            playerRB.velocity = new Vector2(horizontal, playerRB.velocity.y);
+            Debug.Log($"velocity = {playerRB.velocity}");
+        }
+
         playerAnim.SetBool("isWalking", horizontal != 0);
         if(horizontal != 0) playerSR.flipX = horizontal < 0;
 
         if (Input.GetButtonDown("Jump")) {
-            playerRB.AddForce(Vector2.up * jumpForce);
+            playerRB.AddForce(Vector2.up * _jumpForce);
         }
     }
 }
